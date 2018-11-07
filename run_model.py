18c5f808912ccc0ffb2f92b7029ccf19030f26de
brandries <andriesvanderwalt@gmail.com>
2018-11-07 09:02:06 +0200

    clean code version

--- run_model.py
+++ run_model.py
@@ -12,25 +12,23 @@ import keras.backend as K
 from argparse import ArgumentParser
 from sklearn.externals import joblib
 
-parser = ArgumentParser()
-parser.add_argument('--input', help='Path to the input CSV')
-parser.add_argument('--submission', help='Path to the submission CSV')
-args = parser.parse_args()
 
-with open(args.input) as file:
-    df = pd.read_csv(file)
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('--input', help='Path to the input CSV')
+    parser.add_argument('--submission', help='Path to the submission CSV')
+    args = parser.parse_args()
+    df = pd.read_csv(args.input)
+    vector = joblib.load('vector.pkl')
+    pipeline = joblib.load('model.pkl')
+    X = df['text']
+    features = vector.transform(X.astype('U'))
+    predictions = pipeline.predict(features)
+    predictions = df.join(pd.DataFrame(predictions))
+    predictions.drop('text', inplace=True, axis=1)
+    predictions.columns = ['review_id', 'stars_1', 'stars_2',
+                           'stars_3', 'stars_4', 'stars_5']
+    predictions.to_csv(args.submission, index=False)
 
-vector = joblib.load('vector.pkl')
-pipeline = joblib.load('model.pkl')
-X = df['text']
-features = vector.transform(X.astype('U'))
-
-predictions = pipeline.predict(features)
-
-predictions = df.join(pd.DataFrame(predictions))
-predictions.drop('text', inplace=True, axis=1)
-
-predictions.columns = ['review_id', 'stars_1', 'stars_2',
-                       'stars_3', 'stars_4', 'stars_5']
-
-predictions.to_csv(args.submission, index=False)
+if __name__ == '__main__':
+    main()
